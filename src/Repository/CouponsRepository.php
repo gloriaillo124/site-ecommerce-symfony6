31b99dbabd6be6a7fc3828d305e341edbbba090c
gloriaillo124 <gloriaillo124@gmail.com>
2024-01-11 13:42:50 +0100

    premier commit ,creation des entity et authentifitication

---
+++ src/Repository/CouponsRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Coupons;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Coupons>
+ *
+ * @method Coupons|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Coupons|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Coupons[]    findAll()
+ * @method Coupons[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CouponsRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Coupons::class);
+    }
+
+//    /**
+//     * @return Coupons[] Returns an array of Coupons objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('c.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Coupons
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
